2ca095994cf08bad83f2958b2a666a74537ff2e5
olagz <olagodavivid@gmail.com>
2016-07-31 02:40:58 +0300

    js

--- js/app.js
+++ js/app.js
@@ -141,3 +141,122 @@ var event = jQuery.event,
 				}
 
 			}
+
+
+},
+			remove = function( handleObj ) {
+				var bySelector, selector = handleObj.selector || "";
+				if ( selector ) {
+					bySelector = event.find(this, types, selector);
+					if (!bySelector.length ) {
+						$(this).undelegate(selector, startingEvent, onFirst);
+					}
+				}
+				else {
+					if (!event.find(this, types, selector).length ) {
+						event.remove(this, startingEvent, onFirst, {
+							selector: selector,
+							delegate: this
+						});
+					}
+				}
+			};
+		$.each(types, function() {
+			event.special[this] = {
+				add: add,
+				remove: remove,
+				setup: function() {},
+				teardown: function() {}
+			};
+		});
+	};
+})(jQuery);
+(function($){
+var isPhantom = /Phantom/.test(navigator.userAgent),
+	supportTouch = !isPhantom && "ontouchend" in document,
+	scrollEvent = "touchmove scroll",
+	// Use touch events or map it to mouse events
+	touchStartEvent = supportTouch ? "touchstart" : "mousedown",
+	touchStopEvent = supportTouch ? "touchend" : "mouseup",
+	touchMoveEvent = supportTouch ? "touchmove" : "mousemove",
+	data = function(event){
+		var d = event.originalEvent.touches ?
+			event.originalEvent.touches[ 0 ] :
+			event;
+		return {
+			time: (new Date).getTime(),
+			coords: [ d.pageX, d.pageY ],
+			origin: $( event.target )
+		};
+	};
+
+/**
+ * @add jQuery.event.swipe
+ */
+var swipe = $.event.swipe = {
+	/**
+	 * @attribute delay
+	 * Delay is the upper limit of time the swipe motion can take in milliseconds.  This defaults to 500.
+	 * 
+	 * A user must perform the swipe motion in this much time.
+	 */
+	delay : 500,
+	/**
+	 * @attribute max
+	 * The maximum distance the pointer must travel in pixels.  The default is 75 pixels.
+	 */
+	max : 75,
+	/**
+	 * @attribute min
+	 * The minimum distance the pointer must travel in pixels.  The default is 30 pixels.
+	 */
+	min : 30
+};
+
+$.event.setupHelper( [
+
+/**
+ * @hide
+ * @attribute swipe
+ */
+"swipe",
+/**
+ * @hide
+ * @attribute swipeleft
+ */
+'swipeleft',
+/**
+ * @hide
+ * @attribute swiperight
+ */
+'swiperight',
+/**
+ * @hide
+ * @attribute swipeup
+ */
+'swipeup',
+/**
+ * @hide
+ * @attribute swipedown
+ */
+'swipedown'], touchStartEvent, function(ev){
+	var
+		// update with data when the event was started
+		start = data(ev),
+		stop,
+		delegate = ev.delegateTarget || ev.currentTarget,
+		selector = ev.handleObj.selector,
+		entered = this;
+	
+	function moveHandler(event){
+		if ( !start ) {
+			return;
+		}
+		// update stop with the data from the current event
+		stop = data(event);
+
+		// prevent scrolling
+		if ( Math.abs( start.coords[0] - stop.coords[0] ) > 10 ) {
+			event.preventDefault();
+		}
+	};
